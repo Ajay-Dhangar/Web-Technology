6edbbcf98880d1f08ea2799e0874ab8f36b31a72
Ajay Dhangar <99037494+Ajay-Dhangar@users.noreply.github.com>
2022-07-06 15:04:26 +0530

    Done

--- Front-End Development/3. JavaScript/6. TensorFlow.js/TensorFlow-LAB/index.js
+++ Front-End Development/3. JavaScript/6. TensorFlow.js/TensorFlow-LAB/index.js
@@ -2,21 +2,21 @@
 import * as tf from "@tensorflow/tfjs"
 
 // Initializing the input
-const geek_input = tf.tensor([1, 2, 9, 6, 5,
+const code_input = tf.tensor([1, 2, 9, 6, 5,
     4, 7, 4, 7, 4, 3
 ], [1, 11], 'int32');
 
 // Printing Input tensor
-console.log('Input tensor: ', geek_input)
+console.log('Input tensor: ', code_input)
 
 // Weight and size for the bincount
-const geek_Weight = tf.tensor(
+const code_Weight = tf.tensor(
     [0, 2, 5, 8, 9, 3, 5, 5, 3, 8, 2]);
 
-const geek_size = 8;
+const code_size = 8;
 
 // Applying bincount to input tensor
-const r = tf.bincount(geek_input, geek_Weight, geek_size)
+const r = tf.bincount(code_input, code_Weight, code_size)
 
 // Printing result
 console.log("Output tensor: ", r)
